eed9deb71cfd8c715479d9fe654c111a6446b74e
John Allen Manacop <allenmanacop.apps@gmail.com>
2019-09-02 11:53:31 +0800

    New Default Activity

--- app/src/main/java/com/example/electronics/SelectCategories.java
+++ app/src/main/java/com/example/electronics/SelectCategories.java
@@ -1,5 +1,6 @@
 package com.example.electronics;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,25 +10,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class SelectCategories extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_categories);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        button = findViewById(R.id.startquiz);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                openMainActivity();
             }
         });
+
+    }
+
+
+    public void openMainActivity(){
+        Intent intent2 = new Intent(this, MainActivity.class);
+        startActivity(intent2);
+
     }
 
 }
